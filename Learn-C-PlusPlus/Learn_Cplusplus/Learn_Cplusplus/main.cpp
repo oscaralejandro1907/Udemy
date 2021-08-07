af1e2c3cd35af7e5565a332ad84150a2b6dadd8a
Oscar Alejandro Hernandez Lopez <oscarale930719@gmail.com>
2021-08-07 17:41:08 -0500

    Initial commit

---
+++ Learn-C-PlusPlus/Learn_Cplusplus/Learn_Cplusplus/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Learn_Cplusplus
+//
+//  Created by Oscar Alejandro Hernández López on 21/06/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
